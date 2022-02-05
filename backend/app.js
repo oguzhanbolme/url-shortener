7de1d1693341409a9e307797f316500159c083ec
oguzhanbolme <oguzhanbolme1@gmail.com>
2022-02-05 20:15:28 +0300

    entry point configured

---
+++ backend/app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const urlRouter = require("./routes/UrlRouter");
+require("dotenv").config();
+
+const PORT = process.env.PORT;
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use("/shortUrl", urlRouter);
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}`);
+});
