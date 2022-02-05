97f0c763ca288e59fa6ff0ff3edf3b994efd6521
oguzhanbolme <oguzhanbolme1@gmail.com>
2022-02-05 22:20:59 +0300

    app.js updated

--- backend/app.js
+++ backend/app.js
@@ -7,7 +7,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use("/shortUrl", urlRouter);
+app.use("/", urlRouter);
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}`);
